1a3fdec21044153488a1583b6fb5d1c260790679
dengrenjie <dengrenjie31@gmail.com>
2017-01-19 17:34:31 +0800

    put forms in the middle

--- index.php
+++ index.php
@@ -14,34 +14,37 @@
         var total = arr.length/2;
         var arr1=arr.slice(total);
     </script>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-<h1>Add Array</h1>
+<div id="mainbody">
+<h1>Add Contestants</h1>
 <form action="addarray.php" method="get">
     <input type="text" id="name" placeholder="name" name="name">
     <input type="text" id="number" placeholder="stuid" name="stunum">
     <input type="submit" value="click">
 </form>
 <br/>
-<h1>Delete Array</h1>
+<h1>Delete Contestants</h1>
 <form action="delarray.php" method="get">
     <input type="text" id="delnum" placeholder="delnum" name="delnum">
     <input type="submit" value="click">
 </form>
 <br/>
-<h1>Search Array</h1>
+<h1>Search Name by Student ID</h1>
 <form action="">
     <input type="text" id="searcharrnum" placeholder="arrnum">
     <input type="text" id="searchresult">
     <input type="button" onclick="searcharr()" value="click">
 </form>
 <br/>
-<h1>Random</h1>
+<h1>Match</h1>
 <form action="">
     <input type="text" id="result1">
     <input type="text" id="result2">
     <input type="button" value="click" onclick="random()">
 </form>
-<a href="info.php">Information</a>
+<h1><a href="info.php">Contestants Information</a></h1>
+</div>
 </body>
 </html>
